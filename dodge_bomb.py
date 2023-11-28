d1f1103c2f7c791c3e1d26cd5a851b9bee7b9acd
C0B22007 <c0b2200787@edu.teu.ac.jp>
2023-11-28 16:53:23 +0900

    追加機能３：ゲームオーバー時の画像切り替え

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1300, 700
+WIDTH, HEIGHT = 1600, 900
 
 delta = {
     pg.K_UP: (0, -5),
@@ -30,6 +30,7 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    
     kk_rct = kk_img.get_rect()  # 練習３：こうかとんSurfaceのRectを抽出
     kk_rct.center = 900, 400  # 練習３：こうかとんの初期座標
     bb_img = pg.Surface((20,20))  # 練習１：透明のSurfaceを作る
@@ -42,12 +43,19 @@ def main():
 
     clock = pg.time.Clock()
     tmr = 0
+    num = 100
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
+            num = 0
+        num += 1
+        if num ==40:
+
             print("Game Over")
+            
             return
         
         key_lst = pg.key.get_pressed()
@@ -56,7 +64,23 @@ def main():
             if key_lst[k]:  # キーが押されたら
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-        
+        """
+        if sum_mv == [+5,0]:
+            kk_img = pg.transform.rotozoom(kk_img, 180, 1.0)
+        if sum_mv == [+5,-5]:
+            kk_img = pg.transform.rotozoom(kk_img, 45, 1.0)
+        if sum_mv == [0, -5]:
+            kk_img = pg.transform.rotozoom(kk_img, 90, 1.0)
+        if sum_mv == [-5,-5]:
+            kk_img = pg.transform.rotozoom(kk_img, 135, 1.0)
+        if sum_mv == [-5,0]:
+            kk_img = pg.transform.rotozoom(kk_img, 0, 1.0)
+        if sum_mv == [-5,5]:                    
+            kk_img = pg.transform.rotozoom(kk_img, 45, 1.0)
+        if sum_mv == [0,+5]:
+            kk_img = pg.transform.rotozoom(kk_img, 90, 1.0)
+        if sum_mv == [+5,+5]:
+            kk_img = pg.transform.rotozoom(kk_img, 135, 1.0) """
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True,True):
@@ -73,6 +97,10 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+        if num < 3:
+            kk_img = pg.image.load("ex02/fig/0.png")
+            kk_img = pg.transform.rotozoom(kk_img,0,2.0)
 
 
 if __name__ == "__main__":
